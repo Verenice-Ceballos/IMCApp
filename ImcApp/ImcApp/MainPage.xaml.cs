79ab0504c6b801c78c2e34aabe558d6ef8fb18eb
Verenice Ceballos Zubieta <14470416@itcampeche.edu.mx>
2021-10-27 23:42:42 -0500

    Agrega el manejo de eventos que da comportamiento a la interfaz de usuario de la aplicación

--- ImcApp/ImcApp/MainPage.xaml.cs
+++ ImcApp/ImcApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImcApp.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,9 +11,70 @@ namespace ImcApp
 {
     public partial class MainPage : ContentPage
     {
+        private CalculadoraImc imc;
+
         public MainPage()
         {
             InitializeComponent();
+            LimpiarIU();
+
         }
+
+        
+
+        private void CalcularButton_Clicked(object sender, EventArgs e)
+        {
+            decimal peso;
+            decimal estatura;
+            bool pesoEsConvertible = decimal.TryParse(PesoEntry.Text, out peso);
+            bool estaturaEsConvertible = decimal.TryParse(EstaturaEntry.Text, out estatura);
+            if (pesoEsConvertible && estaturaEsConvertible)
+            {
+                CalculadoraImc cimc = new CalculadoraImc(peso, estatura);
+                ImcLabel.Text = string.Format("{0:F4}", cimc.Imc);
+                SituacionNutricionalLabel.Text =
+                    GetEstadoNutricional(cimc.SituacionNutricional);
+            }
+            else
+            {
+                DisplayAlert(
+                    "Alerta",
+                      "El peso y la estatura deben ser valores numéricos.",
+                "Aceptar"
+            );
+            }
+        }
+
+        private void LimpiarButton_Clicked(object sender, EventArgs e)
+        {
+            LimpiarIU();
+        }
+        private string GetEstadoNutricional(CalculadoraImc.EstadoNutricional estado)
+        {
+            switch (estado)
+            {
+                case CalculadoraImc.EstadoNutricional.PesoBajo:
+                    return "Peso bajo";
+                case CalculadoraImc.EstadoNutricional.PesoNormal:
+                    return "Peso normal";
+                case CalculadoraImc.EstadoNutricional.SobrePeso:
+                    return "Sobrepeso";
+                case CalculadoraImc.EstadoNutricional.Obesidad:
+                    return "Obesidad";
+                case CalculadoraImc.EstadoNutricional.ObesidadExtrema:
+                    return "Obesidad extrema";
+                default:
+                    return string.Empty;
+            }
+        }
+        private void LimpiarIU()
+        {
+            PesoEntry.Text = string.Empty;
+            EstaturaEntry.Text = string.Empty;
+            ImcLabel.Text = string.Empty;
+            SituacionNutricionalLabel.Text = string.Empty;
+        }
+
     }
 }
+
